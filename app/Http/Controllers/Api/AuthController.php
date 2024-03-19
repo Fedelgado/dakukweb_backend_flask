6187e2042aa652df4129f856da450f13ee886794
Fede <fedelva8@gmail.com>
2024-03-19 16:18:20 -0300

    did some work on flask routes

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace App\Http\Controllers\Api;
-
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\LoginRequest;
 use App\Http\Resources\Api\UserResource;
--- flask/main.py
+++ flask/main.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, request, render_template
+from markupsafe import escape
 
 app = Flask(__name__)
 @app.route('/')
@@ -8,19 +9,30 @@ def root():
 
 @app.route('/users/<user_id>')
 def get_user(user_id):
-    user = {'id':user_id, 'name':"test", 'phone':"222-222-5124"}
+    user = {'id':user_id,'name':"test", 'phone':"222-222-5124"}
     #users/2654?query=query_test
     query = request.args.get("query")
     if query:
         user["query"] = query
-    return jsonify(user), 200
+    return jsonify(user), 200   
 
+@app.route('/user/<name>')
+def greet(name):
+    return "Bienvenido " + name + ", sos un usuario!!!"
+
+@app.route('/user/<name>/Dashboard')
+def dshBrd(name):
+    return "Aca vas a poder ver todas las cosas divertidas que tengas "+ name +""
+
+@app.route('/user/<name>/escapate')
+def asde(name):
+    return f"ESCAPATE!!{escape(name)}"
+    
 @app.route('/users', methods=["POST"])
 def create_user():
     data = request.get_json()
     data["status"] = "user created"
     return jsonify(data), 201
-
 
 """ @app.route('/penes')
 def penecito():
@@ -31,8 +43,8 @@ def penecito():
 def showindex():
     return render_template('../resources/views/welcome.blade.php')
 
-
-
-
 if __name__ == '__main__':
     app.run(debug=True)
+
+# flask --app <name> run 
+# if wanted, use "--debug" after run 
